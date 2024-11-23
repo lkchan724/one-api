163946220ee40b0a3912b6d6bbc46a4f48a29346
chensenhua <chensenhua@kuaishou.com>
2024-11-23 14:16:31 +0800

    repace error name
    Signed-off-by: chensenhua <chensenhua@kuaishou.com>

--- relay/adaptor/common.go
+++ relay/adaptor/common.go
@@ -1,6 +1,8 @@
 package adaptor
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func SetupCommonRequestHeader(c *gin.Context, req *http.Request, meta *meta.Meta) {
@@ -19,11 +22,52 @@ func SetupCommonRequestHeader(c *gin.Context, req *http.Request, meta *meta.Meta
 		req.Header.Set("Accept", "text/event-stream")
 	}
 }
+func removeNameFromSystemRoles(reader io.Reader) (io.Reader, error) {
+	// 先将io.Reader中的JSON数据解析为map[string]interface{}
+	var data map[string]interface{}
+	decoder := json.NewDecoder(reader)
+	err := decoder.Decode(&data)
+	if err!= nil {
+		return nil, err
+	}
+
+	// 假设JSON中有"messages"数组，且数组元素里包含role字段，进行相应处理
+	if messages, ok := data["messages"].([]interface{}); ok {
+		for _, msg := range messages {
+			if msgMap, ok := msg.(map[string]interface{}); ok {
+				if role, ok := msgMap["role"].(string); ok && role == "system" {
+					// 如果role是system，删除name字段
+					delete(msgMap, "name")
+				}
+			}
+		}
+	}
+
+	// 将修改后的数据重新序列化为JSON格式的字节切片
+	newJsonBytes, err := json.Marshal(data)
+	if err!= nil {
+		return nil, err
+	}
+
+	// 将字节切片包装成io.Reader返回
+	return bytes.NewReader(newJsonBytes), nil
+}
 
 func DoRequestHelper(a Adaptor, c *gin.Context, meta *meta.Meta, requestBody io.Reader) (*http.Response, error) {
 	fullRequestURL, err := a.GetRequestURL(meta)
 	if err != nil {
 		return nil, fmt.Errorf("get request url failed: %w", err)
+	}
+	isXAi := strings.Contains(fullRequestURL, "api.x.ai")
+	logger.SysLogf("fullRequestURL="+fullRequestURL)
+	if isXAi {
+		newBody, err := removeNameFromSystemRoles(requestBody)
+		if err != nil {
+			logger.SysLogf("Request replace error ", err)
+		} else {
+			logger.SysLogf("Request replace success ", err)
+			requestBody = newBody
+		}
 	}
 	req, err := http.NewRequest(c.Request.Method, fullRequestURL, requestBody)
 	if err != nil {
